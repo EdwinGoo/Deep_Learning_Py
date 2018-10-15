e4525cbfc7d2cf5bc6033c7e7cc2b2df92b9a329
EdwinGoo <kkumong9@gmail.com>
2018-10-15 10:47:50 +0900

    reboot

---
+++ DAY_04_reboot/d01_numpy_practice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# normal double
+normdata = list(range(10))
+print(normdata)
+doubledata = [n * 2 for n in normdata ]
+print(doubledata)
+
+# numpy double
+npdata = [[0,1],[1,0]]
+# print(type(arraydata)) ndarray
+arraydata = np.array(npdata)
+print(arraydata)
+print(arraydata*2)
+
+
+
